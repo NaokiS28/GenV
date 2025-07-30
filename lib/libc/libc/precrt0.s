a8d709b242fe78c62787a952a1243705c9d4c0c3
NaokiS28 <biz@nsaito.co.uk>
2025-07-30 17:55:06 +0100

    Fix PSX debugging

---
+++ lib/libc/libc/precrt0.s
@@ -0,0 +1,26 @@
+# ps1-bare-metal - (C) 2023-2025 spicyjpeg
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+# REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+# AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+# INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+# LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+# OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+# PERFORMANCE OF THIS SOFTWARE.
+
+.set noreorder
+
+.set STACK_TOP, 0x801fff00
+
+.section .text._startWrapper, "ax", @progbits
+.global _startWrapper
+.type _startWrapper, @function
+
+_startWrapper:
+    lui   $sp, %hi(STACK_TOP)
+    j     _start
+    addiu $sp, %lo(STACK_TOP)
