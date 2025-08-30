993f81e5c1856dd5eacd6e0d37471e552e869721
NaokiS28 <biz@nsaito.co.uk>
2025-08-30 21:06:19 +0100

    Disable unused formats for STB Image

--- src/genv_common/common/vendor/vendor_conf.h
+++ src/genv_common/common/vendor/vendor_conf.h
@@ -20,8 +20,13 @@
 // STB Image
 #define STBI_NO_STDIO
 
+#define STBI_NO_JPEG
+#define STBI_NO_PSD
+#define STBI_NO_HDR
+#define STBI_NO_PIC
+#define STBI_NO_PNM
+
 #ifdef GENV_PSX
 #define STBI_NO_LINEAR
-#define STBI_NO_HDR
 #define STBI_NO_THREAD_LOCALS
 #endif
