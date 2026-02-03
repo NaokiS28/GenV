dd8cb412ebc138f79d341395f087abb2299d12e5
NaokiS28 <NaokiS28>
2026-02-03 10:24:12 +0000

    SIO0 ACK IRQ is not clock aligned. Account for that with A/B comparison

--- src/genv_hw/psx/common/system/sys.c
+++ src/genv_hw/psx/common/system/sys.c
@@ -18,139 +18,170 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include "sys.h"
+#include "psx/common/registers.hpp"
 
-#define BIOS_ENTRY_POINT ((VoidFunction)   0xbfc00000)
-#define BIOS_API_TABLE   ((VoidFunction *) 0x80000200)
-#define BIOS_BP_VECTOR   ((uint32_t *)     0x80000040)
-#define BIOS_EXC_VECTOR  ((uint32_t *)     0x80000080)
+#define BIOS_ENTRY_POINT ((VoidFunction)0xbfc00000)
+#define BIOS_API_TABLE ((VoidFunction *)0x80000200)
+#define BIOS_BP_VECTOR ((uint32_t *)0x80000040)
+#define BIOS_EXC_VECTOR ((uint32_t *)0x80000080)
 
 /* Internal state */
 
 static uint32_t _savedBreakpointVector[4];
 static uint32_t _savedExceptionVector[4];
-static Thread   _mainThread;
+static Thread _mainThread;
 
-ArgFunction interruptHandler     = 0;
-void        *interruptHandlerArg = 0;
+ArgFunction interruptHandler = 0;
+void *interruptHandlerArg = 0;
 
 Thread *currentThread = &_mainThread;
-Thread *nextThread    = &_mainThread;
+Thread *nextThread = &_mainThread;
 
 /* Exception handler setup */
 
-static inline void _flushCache(void) {
-	// This is the only function that must always run from the BIOS ROM as it
-	// temporarily disables main RAM.
-	BIOS_API_TABLE[0x44]();
+static inline void _flushCache(void)
+{
+    // This is the only function that must always run from the BIOS ROM as it
+    // temporarily disables main RAM.
+    BIOS_API_TABLE[0x44]();
 }
 
 void _exceptionVector(void);
 
-void psx_installExceptionHandler(void) {
-	// Clear all pending IRQ flags and prevent the interrupt controller from
-	// generating further IRQs.
-	IRQ_MASK = 0;
-	IRQ_STAT = 0;
-	DMA_DPCR = 0;
-	DMA_DICR = DMA_DICR_CH_STAT_BITMASK;
+void psx_installExceptionHandler(void)
+{
+    // Clear all pending IRQ flags and prevent the interrupt controller from
+    // generating further IRQs.
+    IRQ_MASK = 0;
+    IRQ_STAT = 0;
+    DMA_DPCR = 0;
+    DMA_DICR = DMA_DICR_CH_STAT_BITMASK;
 
-	// Disable interrupts and the GTE at the COP0 side.
-	cop0_setSR(COP0_SR_CU0);
+    // Disable interrupts and the GTE at the COP0 side.
+    cop0_setSR(COP0_SR_CU0);
 
-	// Overwrite the default breakpoint and exception handlers placed into RAM
-	// by the BIOS with a function that will jump to our custom handler.
-	__builtin_memcpy(_savedBreakpointVector, BIOS_BP_VECTOR,  16);
-	__builtin_memcpy(_savedExceptionVector,  BIOS_EXC_VECTOR, 16);
-	__builtin_memcpy(BIOS_BP_VECTOR,  &_exceptionVector, 16);
-	__builtin_memcpy(BIOS_EXC_VECTOR, &_exceptionVector, 16);
-	_flushCache();
+    // Overwrite the default breakpoint and exception handlers placed into RAM
+    // by the BIOS with a function that will jump to our custom handler.
+    __builtin_memcpy(_savedBreakpointVector, BIOS_BP_VECTOR, 16);
+    __builtin_memcpy(_savedExceptionVector, BIOS_EXC_VECTOR, 16);
+    __builtin_memcpy(BIOS_BP_VECTOR, &_exceptionVector, 16);
+    __builtin_memcpy(BIOS_EXC_VECTOR, &_exceptionVector, 16);
+    _flushCache();
 
-	DMA_DPCR = 0x0bbbbbbb;
-	DMA_DICR = DMA_DICR_IRQ_ENABLE;
+    DMA_DPCR = 0x0bbbbbbb;
+    DMA_DICR = DMA_DICR_IRQ_ENABLE;
 
-	// Ensure interrupt masking is set up properly and the GTE is enabled at the
-	// COP0 side.
-	cop0_setSR(COP0_SR_Im2 | COP0_SR_CU0 | COP0_SR_CU2);
+    // Ensure interrupt masking is set up properly and the GTE is enabled at the
+    // COP0 side.
+    cop0_setSR(COP0_SR_Im2 | COP0_SR_CU0 | COP0_SR_CU2);
 }
 
-void psx_uninstallExceptionHandler(void) {
-	// Disable interrupts at the COP0 side.
-	cop0_setSR(COP0_SR_CU0 | COP0_SR_CU2);
+void psx_uninstallExceptionHandler(void)
+{
+    // Disable interrupts at the COP0 side.
+    cop0_setSR(COP0_SR_CU0 | COP0_SR_CU2);
 
-	// Clear all pending IRQ flags and prevent the interrupt controller from
-	// generating further IRQs.
-	IRQ_MASK = 0;
-	IRQ_STAT = 0;
-	DMA_DPCR = 0;
-	DMA_DICR = DMA_DICR_CH_STAT_BITMASK;
+    // Clear all pending IRQ flags and prevent the interrupt controller from
+    // generating further IRQs.
+    IRQ_MASK = 0;
+    IRQ_STAT = 0;
+    DMA_DPCR = 0;
+    DMA_DICR = DMA_DICR_CH_STAT_BITMASK;
 
-	// Restore the original BIOS breakpoint and exception handlers.
-	__builtin_memcpy(BIOS_BP_VECTOR,  _savedBreakpointVector, 16);
-	__builtin_memcpy(BIOS_EXC_VECTOR, _savedExceptionVector,  16);
-	_flushCache();
+    // Restore the original BIOS breakpoint and exception handlers.
+    __builtin_memcpy(BIOS_BP_VECTOR, _savedBreakpointVector, 16);
+    __builtin_memcpy(BIOS_EXC_VECTOR, _savedExceptionVector, 16);
+    _flushCache();
 }
 
-void psx_setInterruptHandler(ArgFunction func, void *arg) {
-	psx_disableInterrupts();
+void psx_setInterruptHandler(ArgFunction func, void *arg)
+{
+    psx_disableInterrupts();
 
-	interruptHandler    = func;
-	interruptHandlerArg = arg;
-	atomic_signal_fence(memory_order_release);
+    interruptHandler = func;
+    interruptHandlerArg = arg;
+    atomic_signal_fence(memory_order_release);
 }
 
-void psx_flushCache(void) {
-	bool enable = psx_disableInterrupts();
+void psx_flushCache(void)
+{
+    bool enable = psx_disableInterrupts();
 
-	_flushCache();
-	if (enable)
-		psx_enableInterrupts();
+    _flushCache();
+    if (enable)
+        psx_enableInterrupts();
 }
 
-void psx_softReset(void) {
-	psx_disableInterrupts();
-	BIOS_ENTRY_POINT();
-	__builtin_unreachable();
+void psx_softReset(void)
+{
+    psx_disableInterrupts();
+    BIOS_ENTRY_POINT();
+    __builtin_unreachable();
 }
 
 /* IRQ acknowledgement */
 
-bool psx_acknowledgeInterrupt(IRQChannel irq) {
-	if (IRQ_STAT & (1 << irq)) {
-		IRQ_STAT = ~(1 << irq);
-		return true;
-	}
+bool psx_acknowledgeInterrupt(IRQChannel irq)
+{
+    uint16_t a, b;
+    if (irq == IRQ_SIO0)
+    {
+        // SIO0 ACK is not clock aligned and not double buffered.
+        a = IRQ_STAT;
+        psx_delayMicrosecondsBusy(5);
+        b = IRQ_STAT;
+    }
+    else
+    {
+        a = IRQ_STAT;
+        b = a;
+    }
 
-	return false;
+    if (a == b && a & (1 << irq))
+    {
+        IRQ_STAT = ~(1 << irq);
+        return true;
+    }
+
+    return false;
 }
 
-bool psx_waitForInterrupt(IRQChannel irq, int timeout) {
-	for (; timeout > 0; timeout -= 10) {
-		if (psx_acknowledgeInterrupt(irq))
-			return true;
+bool psx_waitForInterrupt(IRQChannel irq, int timeout)
+{
+    for (; timeout > 0; timeout -= 10)
+    {
+        if (psx_acknowledgeInterrupt(irq))
+            return true;
 
-		psx_delayMicrosecondsBusy(10);
-	}
+        if (irq == IRQ_SIO0)
+            psx_delayMicrosecondsBusy(5); // psx_ackInt will wait 5 micros.
+        else
+            psx_delayMicrosecondsBusy(10);
+    }
 
-	return false;
+    return false;
 }
 
-bool psx_waitForDMATransfer(DMAChannel dma, int timeout) {
-	for (; timeout > 0; timeout -= 10) {
-		if (!(DMA_CHCR(dma) & DMA_CHCR_ENABLE))
-			return true;
+bool psx_waitForDMATransfer(DMAChannel dma, int timeout)
+{
+    for (; timeout > 0; timeout -= 10)
+    {
+        if (!(DMA_CHCR(dma) & DMA_CHCR_ENABLE))
+            return true;
 
-		psx_delayMicrosecondsBusy(10);
-	}
+        psx_delayMicrosecondsBusy(10);
+    }
 
-	return false;
+    return false;
 }
 
 /* Thread switching */
 
-void psx_switchThread(Thread *thread) {
-	if (!thread)
-		thread = &_mainThread;
+void psx_switchThread(Thread *thread)
+{
+    if (!thread)
+        thread = &_mainThread;
 
-	nextThread = thread;
-	atomic_signal_fence(memory_order_release);
+    nextThread = thread;
+    atomic_signal_fence(memory_order_release);
 }
